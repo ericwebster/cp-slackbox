d7882ed515ecf997abc79ecd42c0a3f2733b9fdb
Eric Webster <ewebster@connellypartners.com>
2016-02-24 18:13:18 -0500

    changed the way messages are returned to slack

--- app.js
+++ app.js
@@ -72,7 +72,16 @@ app.post('/store', function(req, res) {
           var track = results[0];
           spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + track.id])
             .then(function(data) {
-              return res.send(req);
+              var response = {
+                  "text": "Track added: *" + track.name + "* by *" + track.artists[0].name + "*",
+                  "response_type": "in_channel",
+                  'attachments': [
+                      {
+                          "text":"by " + req.user
+                      }
+                  ]
+              }
+              return res.send(response);
             }, function(err) {
               return res.send(err.message);
             });
